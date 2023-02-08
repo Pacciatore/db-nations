88f119e3108225330f0ae588e495a88b01275506
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-08 16:13:52 +0100

    [BONUS] Realizzazione Bonus con dati del 2017

--- src/org/lessons/java/nations/Main.java
+++ src/org/lessons/java/nations/Main.java
@@ -1,5 +1,6 @@
 package org.lessons.java.nations;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -17,15 +18,15 @@ public class Main {
 
 		milestone2(url, user, password);
 
-		milestone3(url, user, password);
+		// milestone3(url, user, password);
+
+		getLanguageByCountryId(url, user, password);
 
 	}
 
 	public static void milestone2(String url, String user, String password) {
 
 		try (Connection con = DriverManager.getConnection(url, user, password)) {
-
-			System.out.println("Connessione effettuata!");
 
 			String sql = "SELECT  c.name AS country_name,c.country_id , r.name AS region_name, cont.name AS continent_name\r\n"
 					+ "FROM countries c, regions r , continents cont\r\n"
@@ -69,8 +70,6 @@ public class Main {
 
 		try (Connection con = DriverManager.getConnection(url, user, password)) {
 
-			System.out.println("Connessione effettuata!");
-
 			String sql = "SELECT  c.name AS country_name,c.country_id , r.name AS region_name, cont.name AS continent_name\r\n"
 					+ "FROM countries c, regions r , continents cont\r\n"
 					+ "WHERE c.region_id = r.region_id AND cont.continent_id = r.continent_id \r\n"
@@ -93,6 +92,76 @@ public class Main {
 						System.out.print(" | ID: " + countryId);
 						System.out.print(" | Regione: " + regionName);
 						System.out.println(" | Continente: " + continentName);
+
+					}
+
+				}
+
+			}
+
+		} catch (SQLException se) {
+			se.printStackTrace();
+		}
+
+		s.close();
+
+	}
+
+	public static void getLanguageByCountryId(String url, String user, String password) {
+
+		System.out.println("----------------------------------------------------");
+		System.out.println("RICERCA LINGUE IN UNA NAZIONE");
+
+		Scanner s = new Scanner(System.in);
+
+		boolean correctFormat = false;
+		int countryQuery = 0;
+
+		do {
+			try {
+				System.out.print("\nInserire ID della nazione da ricercare (da 1 a 239): ");
+				countryQuery = Integer.parseInt(s.nextLine());
+				correctFormat = true;
+			} catch (Exception e) {
+				System.out.println("Inserire un numero intero!");
+				correctFormat = false;
+			}
+		} while (!correctFormat);
+
+		try (Connection con = DriverManager.getConnection(url, user, password)) {
+
+			String sql = "SELECT l.`language` AS country_language  , cs.population AS country_population, cs.gdp AS country_gdp, c.name AS country_name\r\n"
+					+ "FROM countries c , country_languages cl , languages l , country_stats cs \r\n"
+					+ "WHERE c.country_id = cl.country_id AND cl.language_id = l.language_id \r\n"
+					+ "	AND cs.country_id  = c.country_id \r\n" + "AND c.country_id = ? AND cs.`year` = 2017\r\n"
+					+ "ORDER BY c.name;";
+
+			try (PreparedStatement statement = con.prepareStatement(sql)) {
+
+				statement.setInt(1, countryQuery);
+
+				try (ResultSet rs = statement.executeQuery()) {
+
+					// Uso del 2017 per avere una sola popolazione e pil
+					System.out.println("\nAnno di riferimento: 2017");
+
+					if (rs.next()) {
+						String countryName = rs.getString("country_name");
+						System.out.println("Nazione ricercata: " + countryName.toUpperCase());
+
+						BigDecimal countryGdp = rs.getBigDecimal("country_gdp");
+						System.out.println("\nPIL: " + countryGdp);
+
+						int countryPopulation = rs.getInt("country_population");
+						System.out.println("Popolazione: " + countryPopulation);
+					}
+
+					System.out.println("\nLingue parlate: ");
+
+					while (rs.next()) {
+
+						String countryLanguage = rs.getString("country_language");
+						System.out.print(countryLanguage + " | ");
 
 					}
 
