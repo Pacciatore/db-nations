fa9b0cac3736bdeb1d1078a74f030f776aa9d90c
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-08 14:40:25 +0100

    Connessione a db_nations

--- src/org/lessons/java/nations/Main.java
+++ src/org/lessons/java/nations/Main.java
@@ -1,9 +1,24 @@
 package org.lessons.java.nations;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		String url = "jdbc:mysql://localhost:3300/db_nations";
+		String user = "root";
+		String password = "root";
+
+		try (Connection connessione = DriverManager.getConnection(url, user, password)) {
+
+			System.out.println("Connessione effettuata!");
+
+		} catch (SQLException se) {
+			se.printStackTrace();
+		}
 
 	}
 
