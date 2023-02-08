87eca2a9f5a1392a1d5006722b3398b8ab857796
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-08 15:11:47 +0100

    realizzazione milestone 3

--- src/org/lessons/java/nations/Main.java
+++ src/org/lessons/java/nations/Main.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -14,7 +15,51 @@ public class Main {
 		String user = "root";
 		String password = "root";
 
-		milestone1(url, user, password);
+		// milestone1(url, user, password);
+
+		Scanner s = new Scanner(System.in);
+
+		System.out.print("Inserire nazione da ricercare: ");
+		String countryQuery = s.nextLine();
+
+		try (Connection con = DriverManager.getConnection(url, user, password)) {
+
+			System.out.println("Connessione effettuata!");
+
+			String sql = "SELECT  c.name AS country_name,c.country_id , r.name AS region_name, cont.name AS continent_name\r\n"
+					+ "FROM countries c, regions r , continents cont\r\n"
+					+ "WHERE c.region_id = r.region_id AND cont.continent_id = r.continent_id \r\n"
+					+ "AND c.name LIKE ? \r\n" + "ORDER BY c.name;";
+
+			try (PreparedStatement statement = con.prepareStatement(sql)) {
+
+				statement.setString(1, "%" + countryQuery + "%");
+
+				try (ResultSet rs = statement.executeQuery()) {
+
+					while (rs.next()) {
+
+						String countryName = rs.getString("country_name");
+						int countryId = rs.getInt("country_id");
+						String regionName = rs.getString("region_name");
+						String continentName = rs.getString("continent_name");
+
+						System.out.print("Nazione: " + countryName);
+						System.out.print(" | ID: " + countryId);
+						System.out.print(" | Regione: " + regionName);
+						System.out.println(" | Continente: " + continentName);
+
+					}
+
+				}
+
+			}
+
+		} catch (SQLException se) {
+			se.printStackTrace();
+		}
+
+		s.close();
 
 	}
 
