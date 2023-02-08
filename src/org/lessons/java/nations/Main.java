84192f2b42c14cd5f6c4ac2cacd67886e5ebc3bb
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-08 14:53:42 +0100

    query e display risultato query di milestone 2

--- src/org/lessons/java/nations/Main.java
+++ src/org/lessons/java/nations/Main.java
@@ -2,6 +2,8 @@ package org.lessons.java.nations;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Main {
@@ -12,9 +14,36 @@ public class Main {
 		String user = "root";
 		String password = "root";
 
-		try (Connection connessione = DriverManager.getConnection(url, user, password)) {
+		try (Connection con = DriverManager.getConnection(url, user, password)) {
 
 			System.out.println("Connessione effettuata!");
+
+			String sql = "SELECT  c.name AS country_name,c.country_id , r.name AS region_name, cont.name AS continent_name\r\n"
+					+ "FROM countries c, regions r , continents cont\r\n"
+					+ "WHERE c.region_id = r.region_id AND cont.continent_id = r.continent_id \r\n"
+					+ "ORDER BY c.name;";
+
+			try (PreparedStatement statement = con.prepareStatement(sql)) {
+
+				try (ResultSet rs = statement.executeQuery()) {
+
+					while (rs.next()) {
+
+						String countryName = rs.getString("country_name");
+						int countryId = rs.getInt("country_id");
+						String regionName = rs.getString("region_name");
+						String continentName = rs.getString("continent_name");
+
+						System.out.print("Nazione: " + countryName);
+						System.out.print(" | ID: " + countryId);
+						System.out.print(" | Regione: " + regionName);
+						System.out.println(" | Continente: " + continentName);
+
+					}
+
+				}
+
+			}
 
 		} catch (SQLException se) {
 			se.printStackTrace();
